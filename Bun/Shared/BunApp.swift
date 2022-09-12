4325b6deb4c8a3e64f39fea89eb6c0cd91c2e789
Ricardo <ricky6791@gmail.com>
2022-09-11 21:35:29 -0600

    Base

---
+++ Bun/Shared/BunApp.swift
@@ -0,0 +1,17 @@
+//
+//  BunApp.swift
+//  Shared
+//
+//  Created by Ricardo Avila on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
